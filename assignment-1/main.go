1cabd29f0a6e133dec92fcaf910d7131a80f9b3b
david <me@dwp.my.id>
2024-03-02 23:55:29 +0700

    add assignment-1

---
+++ assignment-1/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Teman struct {
+	Nama      string
+	Alamat    string
+	Pekerjaan string
+	Alasan    string
+}
+
+func main() {
+	// input dari terminal
+	input := os.Args
+	if len(input) < 2 {
+		fmt.Println("Input tidak sesuai.")
+		return
+	}
+	
+	// ubah input jadi integer
+	absen, err := strconv.Atoi(input[1])
+	if err != nil {
+		fmt.Println("Input tidak valid.")
+		return
+	}
+
+	// data teman
+	data := []Teman{
+		{
+			Nama:      "Agus",
+			Alamat:    "Jalan Raya Utara 01",
+			Pekerjaan: "Software Engineer",
+			Alasan:    "Golang untuk membuat aplikasi backend",
+		},
+		{
+			Nama:      "Badur",
+			Alamat:    "Jalan Raya Selatan 02",
+			Pekerjaan: "Software Engineer",
+			Alasan:    "Golang untuk membuat aplikasi backend",
+		},
+		{
+			Nama:      "Hilman",
+			Alamat:    "Jalan Raya Barat 03",
+			Pekerjaan: "Software Engineer",
+			Alasan:    "Golang untuk membuat aplikasi backend",
+		},
+		{
+			Nama:      "Zulfikar",
+			Alamat:    "Jalan Raya Timur 04",
+			Pekerjaan: "Software Engineer",
+			Alasan:    "Golang untuk membuat aplikasi backend",
+		},
+	}
+
+	// print data teman sesuai dengan input
+	if absen > 0 && absen <= len(data) {
+		fmt.Println("Data teman dengan absen no:", absen)
+		fmt.Println("--------")
+		fmt.Println("Nama: ", data[absen-1].Nama)
+		fmt.Println("Alamat: ", data[absen-1].Alamat)
+		fmt.Println("Pekerjaan: ", data[absen-1].Pekerjaan)
+		fmt.Println("Alasan memilih kelas Golang: ", data[absen-1].Alasan)
+	} else {
+		fmt.Println("Data absen peserta tidak ditemukan")
+	}
+
+}
