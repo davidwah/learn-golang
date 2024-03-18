42b940dce5f2bef721e79ad5719c4c85199c1bc6
david <me@dwp.my.id>
2024-03-19 01:39:47 +0700

    update struct model

--- rest-api/models/item.go
+++ rest-api/models/item.go
@@ -1,14 +1,10 @@
 package models
 
-import "time"
-
-type OrderItem struct {
+type Item struct {
 	//gorm.Model
-	ID          uint      `gorm:"primaryKey" json:"-"`
-	OrderID     uint      `json:"-"`
-	ItemCode    string    `json:"itemCode"`
-	Description string    `gorm:"not null;type:varchar(150)"`
-	Quantity    int       `json:"quantity"`
-	CreatedAt   time.Time `json:"-"`
-	UpdatedAt   time.Time `json:"-"`
+	ID          uint   `gorm:"primaryKey" json:"-"`
+	OrderID     uint   `json:"-"`
+	Code        string `json:"itemCode"`
+	Description string `json:"description"`
+	Quantity    int    `json:"quantity"`
 }
--- rest-api/models/order.go
+++ rest-api/models/order.go
@@ -1,17 +1,13 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
 type Order struct {
 	//gorm.Model
-	ID           uint           `gorm:"primaryKey" json:"id"`
-	OrderedAt    time.Time      `json:"orderedAt"`
-	CustomerName string         `json:"customerName"`
-	Items        []OrderItem    `gorm:"foreignKey:OrderID" json:"items"`
-	CreatedAt    time.Time      `json:"-"`
-	UpdatedAt    time.Time      `json:"-"`
-	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	OrderedAt    time.Time `json:"orderedAt"`
+	CustomerName string    `json:"customerName"`
+	Items        []Item    `gorm:"foreignKey:OrderID" json:"items"`
 }
